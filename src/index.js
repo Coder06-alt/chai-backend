9b8bf064ec68b91e960708fdc22cd5969b2de7cb
Coder06-alt <srushtynarayankar27@gmail.com>
2026-07-03 14:05:16 +0530

    api handling

--- src/index.js
+++ src/index.js
@@ -5,7 +5,17 @@ dns.setServers(['8.8.8.8', '8.8.4.4']);
 import 'dotenv/config'
 
 import connectDB from '../src/db/index.js'
+
 connectDB()
+.then(() =>{
+    app.listen(process.env.PORT || 8000, () => {
+        consol.log(`Server is running at port: ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("MONGODB DB connection failed !!! ",err)
+})
+
 /*
 import express from "express"
 const app = express()
